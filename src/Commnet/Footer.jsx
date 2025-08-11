f1eda39cde1ea804036577ab74345871b0035852
Dipto842 <diptobakshi72@gmail.com>
2025-08-11 13:41:24 +0600

    responsive ipad mini done

--- src/Commnet/Footer.jsx
+++ src/Commnet/Footer.jsx
@@ -27,11 +27,11 @@ Community Now.</h1>
     <h1 className='font text_right  lg:text-[60px] text-white lg:mt-0 lg:w-[485px]'>contact.skincare.com</h1>
 
   
-  <div className='lg:mt-52'>
-    <div className=' lg:flex items-center lg:justify-between text-white mt-10 lg:w-[485px]  sosar '>
-     <a href="">Terms of Service</a>        
-      <a href="/">Privacy Policy</a>
-      <a href="/">Cookies Policy</a>
+  <div className='lg:mt-52 '>
+    <div className=' lg:flex items-center lg:justify-between text-white mt-10 lg:w-[485px]   sosar '>
+     <button className=' focus:cursor-pointer ' >Terms of Service</button>        
+      <button >Privacy Policy</button>
+      <button >Cookies Policy</button>
      
    </div>
   </div>
@@ -50,7 +50,7 @@ Community Now.</h1>
 
 
                <div className='marjin_right'>
-                 <h1 className=' lg:text-[339px] last   lg:-left-[6rem] text-[#34423e]  lg:-top-[6rem] lg:relative  font_bold_700'>
+                 <h1 className=' lg:text-[339px] last    lg:-left-[6rem] text-[#34423e]  lg:-top-[6rem] lg:relative  font_bold_700'>
                    
                     SKINCARE
                 </h1>
